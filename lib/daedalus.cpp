e5e2ed90b2f1d70e0868121ae448a0512654b347
danielaugusto <daniel.augusto191@gmail.com>
2023-11-10 19:24:09 -0300

    remaking

--- lib/daedalus.cpp
+++ lib/daedalus.cpp
@@ -8,28 +8,27 @@
 using namespace llvm;
 #include <llvm/Pass.h>
 #include <iostream>
-int i = 1;
 namespace Daedalus {
 AnalysisKey DaedalusAnalysis::Key;
 DaedalusAnalysis::Result DaedalusAnalysis::run(Function &F,FunctionAnalysisManager &FAM) {
 	std::unordered_map<Instruction *, Function *> instr_Func;
-	
+	return instr_Func;
+}
+PreservedAnalyses DaedalusPass::run(Function &F, FunctionAnalysisManager &FAM) {
+	std::unordered_map<Instruction *, Function *> instr_Func;
+	std::set<Instruction> toMap;
+	for(Instruction &I: instructions(F)) toMap.insert(I);
 	for(Instruction &I: instructions(F)){
 		if(!I.willReturn() || I.mayThrow()) continue;
+		if(isa<AllocaInst>(I)) continue;
 		ProgramSlice *ps = new ProgramSlice(I,F);
-		if(i) {i=0;continue;} 
-		// TODO: The point here is, the slices seems are generated fine but they cannot be outlined. I think some slices can not be outlined, seem that slices such that instruction criteria may throw or not have a return are example of it. what others kind of slices can not be outlined.
-
 		//if(ps->newcanOutline()){
 			Function *X = ps->outline();
 		//	instr_Func.insert({&I, X});
 		//}
 		delete(ps);
 	}
-	return instr_Func;
-}
-PreservedAnalyses DaedalusPass::run(Function &F, FunctionAnalysisManager &FAM) {
-	auto &FunctionArray = FAM.getResult<DaedalusAnalysis>(F);
+	//auto &FunctionArray = FAM.getResult<DaedalusAnalysis>(F);
 	
 	return PreservedAnalyses::all();
 }
