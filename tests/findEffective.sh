81cb7930b7c58fc3a64db169c2f448d58fb787fd
Casperento <44746868+Casperento@users.noreply.github.com>
2024-09-04 16:14:31 -0300

    findEffective and gentest refact.

--- tests/findEffective.sh
+++ tests/findEffective.sh
@@ -12,16 +12,20 @@ for i in "$SRCFOLDER"/*.ll; do
 
         printf "\nTest name: %s\n" "${FILENAME}"
         
-        opt -load-pass-plugin=/home/rafackstein/src/github/Daedalus/build/lib/libdaedalus.so -passes=daedalus -S "${FILENAME}" -o "${NEWFILENAME}" 2> /dev/null
+        opt -load-pass-plugin=../build/lib/libdaedalus.so -passes=daedalus -S "${FILENAME}" -o "${NEWFILENAME}" 2> /dev/null
 
-        if [ "$(grep sliceclone "${FILENAME}")" ]; then
-            # Compare number of instructions
-            INSTCOUNT_B=$(opt -disable-output -stats -passes=instcount "${FILENAME}" 2>&1 | awk "/of all types/{print}" | grep -Eo '^[0-9]*\s')
-            INSTCOUNT_A=$(opt -disable-output -stats -passes=instcount "${NEWFILENAME}" 2>&1 | awk "/of all types/{print}" | grep -Eo '^[0-9]*\s')
+        # Compare number of instructions
+        INSTCOUNT_B=$(opt -disable-output -stats -passes=instcount "${FILENAME}" 2>&1 | awk "/of all types/{print}" | grep -Eo '^[0-9]*\s')
+        INSTCOUNT_A=$(opt -disable-output -stats -passes=instcount "${NEWFILENAME}" 2>&1 | awk "/of all types/{print}" | grep -Eo '^[0-9]*\s')
+        printf "Number of instructions before Daedalus: %s\n" "${INSTCOUNT_B}"
+        printf "Number of instructions after: %s\n" "${INSTCOUNT_A}"
+
+        if [ "$(grep sliceclone "${NEWFILENAME}")" ]; then
             if [ "${INSTCOUNT_A}" -lt "${INSTCOUNT_B}" ]; then
-                printf "The test's number of instructions after Daedalus is less than before!\n"
-                printf "Number of instructions before: %s\n" "${INSTCOUNT_B}"
-                printf "Number of instructions after: %s\n" "${INSTCOUNT_A}"
+                printf "\033[32mThe number of instructions after Daedalus is LESS than before!\033[0m\n"
+            fi
+            if [ "${INSTCOUNT_A}" -gt "${INSTCOUNT_B}" ]; then
+                printf "\033[31mThe number of instructions after Daedalus is GREATER than before...\033[0m\n"
             fi
         fi
     fi
--- tests/gentest.sh
+++ tests/gentest.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 clang -S -Xclang -disable-O0-optnone -emit-llvm "$1.c" -o "$1.ll"
-cp "$1.ll" "allLL/$1.ll"
-opt -S -passes=mem2reg "$1.ll" -o "$1.ll"
-opt -S -passes=lcssa "$1.ll" -o "$1.ll"
+
+if [ -f "$1.ll" ]; then
+    opt -S -passes=mem2reg "$1.ll" -o "$1.ll"
+    opt -S -passes=lcssa "$1.ll" -o "$1.ll"
+else
+    printf "\n%s did not compile...\n\n" "$1.c"
+fi
