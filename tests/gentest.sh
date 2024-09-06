051b2543fc6d9bcedb0748f3c92820acff9906cf
Casperento <44746868+Casperento@users.noreply.github.com>
2024-09-06 13:45:19 -0300

    resource files removed

--- tests/gentest.sh
+++ tests/gentest.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#
+#   Script to generate .ll files for each test's source file
+#
+set -e
+
 clang -S -Xclang -disable-O0-optnone -emit-llvm "$1.c" -o "$1.ll"
-opt -S -passes=mem2reg "$1.ll" -o "$1.ll"
-opt -S -passes=lcssa "$1.ll" -o "$1.ll"
+
+opt -S -passes=mem2reg,lcssa "$1.ll" -o "$1.ll"
+
+opt -debug-only=daedalus -passes=daedalus -load-pass-plugin=../build/lib/libdaedalus.so "$1.ll" -o "$1.d.ll" 2>&1
